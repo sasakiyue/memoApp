03c28ca1a04de58c1cae2e6efcef7716faa56138
Yo Sasaki <yws10lsee@gmail.com>
2018-06-12 19:01:05 +0900

    first push

--- App.js
+++ App.js
@@ -5,7 +5,40 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello!</Text>
+
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイディア</Text>
+            <Text style={styles.memoDate}>2018/05/26</Text>
+          </View>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイディア</Text>
+            <Text style={styles.memoDate}>2018/05/26</Text>
+          </View>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイディア</Text>
+            <Text style={styles.memoDate}>2018/05/26</Text>
+          </View>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイディア</Text>
+            <Text style={styles.memoDate}>2018/05/26</Text>
+          </View>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイディア</Text>
+            <Text style={styles.memoDate}>2018/05/26</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoAddBottom}>
+          <Text style={styles.memoAddBottomTitle}>+</Text>
+        </View>
+
       </View>
     );
   }
@@ -14,8 +47,68 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#827f7f',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    height: 78,
+    left: 0,
+    right: 0,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  memoAddBottom: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  memoAddBottomTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
   },
 });
